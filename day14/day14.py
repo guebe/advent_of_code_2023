67606662262eb055adca8cac9f7a567cc2749245
Guenter Ebermann <guenter.ebermann@googlemail.com>
2023-12-14 09:35:06 +0100

    day14 refactor

--- day14/day14.py
+++ day14/day14.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-def weight(x):
-    return sum(len(x)-i if s=='O' else 0 for i,s in enumerate(x))
-
 def slide(x):
+    weight = 0
     o, e, n = [], [], []
     for s in x:
         if s == '#':
+            weight += sum(len(x) - len(n) - i for i, _ in enumerate(o))
             n += o + e + ['#']
             o, e = [], []
         elif s == '.':
             e.append('.')
         elif s == 'O':
             o.append('O')
-    return n + o + e
+    weight += sum(len(x) - len(n) - i for i, _ in enumerate(o))
+    return weight, n + o + e
 
 def slide_array(a):
-    return sum(weight(slide(x)) for x in a)
+    return sum(slide(x)[0] for x in a)
 
 a = np.array([list(line.strip()) for line in open("input").readlines()])
 a = np.transpose(a)
