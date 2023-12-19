7d78d74cb02ea4d3ad4cbec5237940dc375dd364
Guenter Ebermann <guenter.ebermann@googlemail.com>
2023-12-19 20:58:25 +0100

    day19 part 1 refactor

--- day19/day19.py
+++ day19/day19.py
@@ -1,58 +1,37 @@
+# This solution uses eval to parse dictionary and to evaluate the conditions.
+# The final condition in each line is encoded as always True to avoid spectial
+# handling.
 import re
 
 def parse_file():
-    directions, inputs = open("input").read().split("\n\n")
-    ins = []
-    for line in inputs.split():
-        dict_ = re.sub('([a-z])=', "'\\1':", line)
-        dict_ = eval(dict_)
-        ins.append(dict_)
+    sect1, sect2 = open("input").read().split("\n\n")
 
-    dirs = {}
-    for line in directions.split():
-        key, values = line.split("{")
-        values = values.replace("}","").split(",")
-        assert len(values) >= 2
-        xxx = []
-        for val in values:
-            if ":" in val:
-                cond, kex = val.split(":")
-            else:
-                cond, kex = None, val
-            xxx.append((cond, kex))
-        dirs[key]=xxx
-    return ins, dirs
+    workflows = {}
+    for line in sect1.split():
+        key, blocks = line.replace("}","").split("{")
+        workflows[key] = [b.split(":") if ":" in b else ("True", b) for b in blocks.split(",")]
 
-def recurse(i, k):
-    start = dirs[k]
-    x, m, a, s = i['x'], i['m'], i['a'], i['s']
+    part_ratings = [eval(re.sub('([a-z])=', "'\\1':", line)) for line in sect2.split()]
 
-    for cond, kex in start:
-        if not cond:
-            if kex == 'A':
+    return workflows, part_ratings
+
+def recurse(part, startkey):
+    for cond, key in workflows[startkey]:
+        x,m,a,s = part['x'], part['m'], part['a'], part['s']
+        if (eval(cond)):
+            if key == 'A':
                 return 1
-            elif kex == 'R':
+            elif key == 'R':
                 return 0
             else:
-                return recurse(i, kex)
+                return recurse(part, key)
         else:
-            if (eval(cond)):
-                if kex == 'A':
-                    return 1
-                elif kex == 'R':
-                    return 0
-                else:
-                    return recurse(i, kex)
-            else:
-                pass
+            pass
+    assert False
 
-ins, dirs = parse_file()
-
-sss = 0
-for i in ins:
-    y = recurse(i, 'in')
-    if y:
-        sss += i['x'] + i['m'] + i['a'] + i['s']
-print(sss)
-
-
+res = 0
+workflows, part_ratings = parse_file()
+for part in part_ratings:
+    if recurse(part, 'in'):
+        res += part['x'] + part['m'] + part['a'] + part['s']
+print(res)
